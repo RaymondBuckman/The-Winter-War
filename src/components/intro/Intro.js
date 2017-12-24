f42c682b188b9204e013582c93899ffcebef887b
Raymond <dude92128@aol.com>
2017-12-23 23:33:23 -0800

    Update Intro.js

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -13,11 +13,10 @@ class Intro extends Component {
                 </video> 
                 <div className="text-container">
                     <h1 id="the-winter-war" className="text-center">The Winter War</h1>
-                    <h1 id="zimhaya-voinya" className="text-center">зимняя война</h1>
+                    <h1 id="talvisota" className="text-center">Talvisota</h1>
+                    <h1 id="zimnyaya-voyna" className="text-center">Зимняя Война</h1>
                     <h2 id="the-story" className="text-center">The story of how Finland held its own against the Soviet Union</h2>
                 </div>
-            </div>
-            <div className="Intro-2">
             </div>
         </div>
         );
