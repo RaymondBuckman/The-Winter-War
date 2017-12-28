a504170996eac333dc5914c8e5c509ed96aa81d8
Raymond <dude92128@aol.com>
2017-12-27 20:56:19 -0800

    Update Intro.js

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -7,14 +7,13 @@ export default class Intro extends Component {
     render() {
         return(
         <div>
-            <div className="Intro-1">
+            <div className="Intro-div">
                 <video preload="preload" id="intro-video" autoPlay="autoplay" loop="loop" muted>
                     <source src={snowVideo} type="video/mp4"></source>
                 </video> 
                 <div className="text-container">
                     <h1 id="the-winter-war" className="text-center">The Winter War</h1>
                     <h1 id="talvisota" className="text-center">Talvisota</h1>
-                    <h1 id="zimnyaya-voyna" className="text-center">Зимняя Война</h1>
                     <h2 id="the-story" className="text-center">The story of how Finland held its own against the Soviet Union</h2>
                 </div>
             </div>
