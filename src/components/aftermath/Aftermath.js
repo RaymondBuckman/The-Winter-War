47d39f40c253f3644adbba2e7a9a5cf2d76e1ac4
Raymond <dude92128@aol.com>
2018-01-02 21:40:16 -0800

    Update Aftermath.js

--- src/components/aftermath/Aftermath.js
+++ src/components/aftermath/Aftermath.js
@@ -6,24 +6,25 @@ export default class Aftermath extends Component {
     render() {
         return(
         <div>
-            <div className="aftermath-div">
+            <div id="aftermath-div" className="aftermath-div">
                 <div className="container">
                     <div className="row">
                         <h1 id="jalkiseuraukset" className="text-center" tabIndex="0">Jälkiseuraukset</h1>
                         <h1 id="aftermath" className="text-center" tabIndex="0">Aftermath</h1>
                     </div>
                     <div className="row">
-                        <div id="stalin" className="col-xs-4 col-xs-offset-0" data-tip data-for="stalin-tooltip"></div>
+                        <div id="vyborg" className="col-xs-4 col-xs-offset-0" data-tip data-for="vyborg-tooltip"></div>
                         <p id="aftermath-paragraph" className="col-xs-7 col-xs-offset-5 text-right">
-                            Stalin was himself suspicious of Finland. He believed that Finland would welcome the defeat of communist Russia – after all, Finland had helped anti-communist groups in the Russian Civil War and Stalin believed that Finland would allow her land to be used as a base by invading forces for an attack on the Soviet Union.<br/><br/>
-                            After discussions in Helsinki, Stalin’s proposals were rejected. Ironically, two people did show some ‘support’ for them – Paasikivi and Marshall Mannerheim, c-in-c of designate of Finland’s military in time of war. Both felt that some islands in the Gulf of Finland should be ceded to ‘buy off’ the Soviets as both feared that if war did occur, Finland would have to fight the entire U.S.S.R. by itself with no help from any other country. However, their beliefs were rejected by the Finnish government, even though Germany urged Finland to accept the Soviet's demands. By the end of November 1939, war between Finland and the U.S.S.R. seemed inevitable.
+                            By the terms of the treaty, Finland lost almost 10% of its national territory. However, Finland also avoided being absorbed into the Soviet Union. On July 31, 1940, Adolf Hitler gave the order to start planning an assault on the Soviet Union.  One month later the Germans allowed the secret sale of weapons to Finland. German and Finnish military authorities made an agreement on September 12, and an official exchange of diplomatic notes was sent on September 22.<br/><br/>
+                            
+                            Despite several Soviet attempts improve relations & reduce German influence, Finland became allies with Nazi Germany & invaded the Soviet Union together in the Continuation war. During this war Finland was able to temporarily recapture Karelia for 3 years until the Soviets drove out Nazi Germany & its allies. Today, the formerly Finnish terrority is under control of the Russian Federation. 
                         </p>
                     </div>
                 </div>
             </div>
-            <ReactTooltip id="stalin-tooltip" place="bottom" type="light"
+            <ReactTooltip id="vyborg-tooltip" place="bottom" type="light"
             effect="float" role="tooltip">
-                <span className="tooltip-span">Joseph Stalin</span>
+                <span className="tooltip-span">Vyborg (formerly Viipuri, Finland),<br/> located in Leningrad Oblast, Russia.</span>
             </ReactTooltip>
         </div>
         );
