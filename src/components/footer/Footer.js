5b17a306271b7d87a7d25c5571c280b75fd431da
Raymond <dude92128@aol.com>
2018-01-23 23:57:09 -0800

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react';
 import {findDOMNode} from 'react-dom';
+import Particles from 'react-particles-js';
+
 
 export default class Footer extends Component {
     render() {
         return(
         <div className="footer-div">
-            <div className="container">
-                
+            <Particles 
+                    params={{
+                        particles: {
+                            number: {
+                                value: 80,
+                            },
+                            size: {
+                                value: 7,
+                                random: true,
+                            },
+                            line_linked: {
+                                enable: false
+                            },
+                            move: {
+                                enable: true,
+                                speed: 7,
+                                direction: "bottom",
+                                random: false,
+                                straight: false,
+                                out_mode: "out",
+                                bounce: false,
+                                attract: {
+                                    enable: false,
+                                    rotateX: 600,
+                                    rotateY: 1200
+                                }
+                            },
+                            interactivity: {
+                                detect_on: "canvas",
+                                events: {
+                                    onhover: {
+                                        enable: true,
+                                        mode: "repulse"
+                                    },
+                                    onclick: {
+                                        enable: true,
+                                        mode: "repulse"
+                                    },
+                                }
+                            }
+                        }
+                    }}
+                className="snow-background"
+            />
+            <div className="container">  
                 <div className="row">
                     <h1 id="lisaa-linkkeja" className="text-center" tabIndex="0">Lisää Linkkejä</h1>
                     <h1 id="more-links" className="text-center" tabIndex="0">More Links</h1>
                 </div>
                 <div className="row">
-                    <ul id="videos-list" className="col-xs-4 col-xs-offset-0 text-center">
+                    <ul id="videos-list" className="col-lg-4 col-lg-offset-0 text-center">
                         <li>Videos</li>
                         <li>
                             <a href="https://www.youtube.com/watch?v=71cZbqtGBJQ" target="_blank" role="link">Soviet Union vs Finland - The Winter War 1939</a>
@@ -20,11 +65,8 @@ export default class Footer extends Component {
                         <li>
                             <a href="https://www.youtube.com/watch?v=lWi2Kclq0DE" target="_blank" role="link">The Winter War: A Soviet Failure</a>
                         </li>
-                        <li>
-                            <a href="https://www.youtube.com/watch?v=yHrndb0oZEc" target="_blank" role="link">Motti Tactics - How The Finns Destroyed Soviet Divisions</a>
-                        </li>
                     </ul>
-                    <ul id="details-list" className="col-xs-4 col-xs-offset-0 text-center">
+                    <ul id="details-list" className="col-lg-4 col-lg-offset-0 text-center">
                         <li>Details</li>
                         <li>
                             <a href="http://www.historylearningsite.co.uk/world-war-two/world-war-two-and-eastern-europe/the-winter-war-1939/the-winter-war-1939/" target="_blank" role="link">Causes of the War</a>
@@ -36,7 +78,7 @@ export default class Footer extends Component {
                             <a href="http://nationalinterest.org/feature/how-finland-lost-world-war-ii-the-soviets-won-peace-17412" target="_blank" role="link">How Finland Lost World War II to the Soviets, But Won Peace</a>
                         </li>
                     </ul>
-                    <ul id="images-list" className="col-xs-4 col-xs-offset-0 text-center">
+                    <ul id="images-list" className="col-lg-4 col-lg-offset-0 text-center">
                         <li>Images</li>
                         <li>
                             <a href="http://www.businessinsider.com/ussr-russia-finald-wwii-winter-war-photos-2017-3/#a-finnish-soldier-at-his-post-on-the-mannerheim-line-in-finland-on-december-14-1939-5" target="_blank" role="link">These 17 photos show Finland's brutally cold World War II battle with the Soviet Union</a>
