22611e11098df5947968d94ebac242e07feca195
Raymond <dude92128@aol.com>
2018-01-23 23:57:28 -0800

    Update HomeFieldAdvantage.js

--- src/components/homeFieldAdvantage/HomeFieldAdvantage.js
+++ src/components/homeFieldAdvantage/HomeFieldAdvantage.js
@@ -12,15 +12,15 @@ export default class HomeFieldAdvantage extends Component {
                         <h1 id="home-field-advantage" className="text-center" tabIndex="0">Home-Field Advantage</h1>
                     </div>
                     <div className="row">
-                        <p id="home-field-advantage-paragraph" className="col-xs-7 col-xs-offset-0 text-left">
+                        <p id="home-field-advantage-paragraph" className="col-lg-7 col-lg-offset-0 text-left">
                             In one sense the Finnish Army was in an excellent position to defend its nation. Finnish troops were trained to use their own terrain to their advantage. Finnish troops were well suited to the forests and snow-covered regions of Finland and they knew the lay of the land. Finnish ski troops were highly mobile and well trained.<br/><br/>     
                             For the Soviet Union it was completely different. It was used to war games on large expanses of open ground. The snow covered forests of Finland were a different matter and the Soviets were to find that they were frequently confined to the area around roads as many of their men were unused to Finland’s terrain and the majority of their vehicles were unable to go off road. Their tactics developed during training did not include such terrain.<br/><br/>
                             The Red Army was ill-equipped for a winter war. Whereas the army was well supplied with standard military equipment, it had little that was required for the snow-covered forests of Finland. White camouflage clothing was not issued and vehicles simply could not cope with the cold. The winter of 1939-40 was particularly severe.
                         </p>
-                        <div id="home-field-advantage-image" className="col-xs-4 col-xs-offset-8">
+                        <div id="home-field-advantage-image" className="col-lg-4 col-lg-offset-8">
                             <div id="finnish-soldiers"></div>
                             <span className="text-container">
-                                <p>The Finns' mobility & knowledge of the terrain allowed them to use Motti tactics against the Soviets <a href="https://www.youtube.com/watch?v=yHrndb0oZEc" target="_blank">(click to view more on Motti tactics...)</a></p>
+                                <p>The Finns' mobility & knowledge of the terrain allowed them to use Motti tactics against the Soviets <a href="https://www.youtube.com/watch?v=yHrndb0oZEc" target="_blank">(view more on Motti tactics...)</a></p>
                             </span>
                         </div>
                     </div>
