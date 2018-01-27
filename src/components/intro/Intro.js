3861e50126f3a0033f86bd6a6706277320a377d0
Raymond <dude92128@aol.com>
2018-01-26 23:54:45 -0800

    Update Intro.js

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -11,10 +11,10 @@ export default class Intro extends Component {
                     <source src={forestVideo} type="video/mp4"></source>
                 </video> 
                 <div className="text-container">
-                    <h1 id="the-winter-war" className="text-center">The Winter War</h1>
+                    <h1 id="the-winter-war" className="text-center" tabIndex="0">The Winter War</h1>
                     <h1 id="talvisota" className="text-center">Talvisota</h1>
                     <h2 id="the-story" className="text-center">The story of how Finland held its own against the Soviet Union</h2>
-                    <h1 id="down-arrow" className="text-center">|<br/>v</h1>
+                    <h1 id="down-arrow" className="text-center" tabIndex="0">|<br/>v</h1>
                 </div>
             </div>
         </div>
