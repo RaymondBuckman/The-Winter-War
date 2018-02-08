fa0c77bfd4c7327e9681accb98aff07435a73980
Raymond <dude92128@aol.com>
2018-02-07 23:55:03 -0800

    Update BufferZone.js

--- src/components/bufferZone/BufferZone.js
+++ src/components/bufferZone/BufferZone.js
@@ -76,20 +76,19 @@ export default class BufferZone extends Component {
                               },
                         }
                     }}
-                className="snow-background"
-            />
+                className="snow-background"/>
                 <div className="container">
                     <div className="row">
                         <h1 id="bufernaya-zona" className="text-center">Буферная Зона</h1>
                         <h1 id="buffer-zone" className="text-center" tabIndex="0">Buffer Zone</h1>
                     </div>
                     <div className="row">
-                        <p id="buffer-zone-paragraph" className="col-lg-7 col-lg-offset-0 text-left">
+                        <p id="buffer-zone-paragraph" className="col-xs-10 col-xs-offset-1 col-lg-7 col-lg-offset-0 text-left">
                             The Soviet Union’s feud with its Nordic neighbor began in 1939, when Soviet leader Joseph Stalin looked to expand his influence over Eastern Europe. At the time Finland had control over Karelia, a large land mass in Finland's east that stretched to just outside of Leningrad (known today as St. Petersburg).<br/><br/>     
                             To defend a possible approach to Leningrad by the Nazis, Stalin wanted Karelia as well as several Finnish islands in the Gulf of Finland as a buffer zone. Throughout its history, Russia has always had a problem defending its western cities from invasion & has used conquered areas as buffer zones <a href="https://www.youtube.com/watch?v=HE6rSljTwdU" target="_blank">(view more on Russia's historical need of buffer zones...)</a>.  He also wanted to lease Hanko as a military base and to establish a garrison of 5,000 men there and he demanded more Finnish land on the Soviet border to be ceded to the U.S.S.R. In return, Stalin offered Finland land in Soviet Karelia and the right for Finland to fortify the Aaland Islands.<br/><br/>    
                             Finland was highly suspicious of anything required by Stalin. Relations between Russia and Finland had been fraught for many decades and nearly everyone in Finland saw Stalin’s demands as an attempt by Russia to re-establish her authority over Finland once again. 
                         </p>
-                        <div id="buffer-zone-image" className="col-lg-4 col-lg-offset-8">
+                        <div id="buffer-zone-image" className="col-lg-4 col-lg-offset-8 hidden-xs" >
                             <img id="karelia-map" src={KareliaImage} alt="Map of Soviet-Finnish border region"></img>
                             <span className="text-container">
                                 <p>Map of Karelia & its proximity to Leningrad <a href="http://helyla.onego.ru/history_eng.html" target="_blank">(read more on the history of Karelia...)</a></p>
