65c22dd9bf8afbb456f634a39cb30c96b62313bc
Raymond <dude92128@aol.com>
2018-02-10 23:45:37 -0800

    Update Aftermath.js

--- src/components/aftermath/Aftermath.js
+++ src/components/aftermath/Aftermath.js
@@ -19,7 +19,7 @@ export default class Aftermath extends Component {
                                 <p>Vyborg (formerly Viipuri, Finland), located in Leningrad Oblast, Russia</p>
                             </span>
                         </div>
-                        <p id="aftermath-paragraph" className="col-lg-7 col-lg-offset-5 text-right">
+                        <p id="aftermath-paragraph" className="col-xs-10 col-xs-offset-1 col-lg-7 col-lg-offset-5 text-right">
                             By the terms of the treaty, Finland lost almost 10% of its national territory. However, Finland also avoided being absorbed into the Soviet Union. On July 31, 1940, Adolf Hitler gave the order to start planning an assault on the Soviet Union.  One month later the Germans allowed the secret sale of weapons to Finland. German and Finnish military authorities made an agreement on September 12, and an official exchange of diplomatic notes was sent on September 22.<br/><br/>
                             
                             Despite several Soviet attempts improve relations & reduce German influence, Finland became allies with Nazi Germany & invaded the Soviet Union together in The Continuation War <a href="http://media.wfyi.org/fireandice/history/continuation40.htm" target="_blank">(read more...)</a>. During this war Finland was able to temporarily recapture Karelia for 3 years until the Soviets drove out Nazi Germany & its allies. Today, the formerly Finnish terrority is under control of the Russian Federation. 
