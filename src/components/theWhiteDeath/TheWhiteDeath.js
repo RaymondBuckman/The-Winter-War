3655a201402f7ed98b836a65f4297086cc6adb03
Raymond <dude92128@aol.com>
2018-02-10 23:47:42 -0800

    Update TheWhiteDeath.js

--- src/components/theWhiteDeath/TheWhiteDeath.js
+++ src/components/theWhiteDeath/TheWhiteDeath.js
@@ -13,7 +13,7 @@ export default class TheWhiteDeath extends Component {
                         <h1 id="the-white-death" className="text-center" tabIndex="0">The White Death</h1>
                     </div>
                     <div className="row">
-                        <p id="the-white-death-paragraph" className="col-lg-7 col-lg-offset-0 text-left">
+                        <p id="the-white-death-paragraph" className="col-xs-10 col-xs-offset-1 col-lg-7 col-lg-offset-0 text-left">
                             Simo Häyhä, dubbed “The White Death” by the Soviets, is widely regarded as the most skilled and successful sniper there ever was, with over 500 kills to his name.<br/><br/>
             
                             During the winter war with his Mosin-Nagant M91 rifle, he would dress in white winter camouflage, and carry with him only a day’s worth of supplies and ammunition. While hiding out in the snow, he would then take out any Soviet who entered his killing zone. Häyhä preferred to use iron sights on his gun instead of scopes, as scopes had a tendency to glare in the sunlight and reveal his position. He would put snow in his mouth to hide his breath from being seen in the cold air. While he may sound like an ordinary sniper, this was far from the case: over the course of 100 days during the winter he racked up over 500 kills, earning him the nickname “The White Death”. The Soviets feared him so much that they mounted numerous counter sniper and artillery attacks to get rid of him, all of which failed miserably. However, on March 6th, 1940, he was hit in the jaw by an explosive round from a counter sniper, disfiguring his face. He fell into an 11-day coma, awakening on the day that the war ended.<br/><br/>
