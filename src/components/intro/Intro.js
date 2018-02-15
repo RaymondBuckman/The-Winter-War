3c6df1cb9972524c47df238d235bbfb514b95f08
Raymond <dude92128@aol.com>
2018-02-14 22:22:28 -0800

    Update Intro.js

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -4,14 +4,14 @@ import forestVideo from '../../img/forest-video.mp4';
 import $ from "jquery";
 
 export default class Intro extends Component {
-    render() {
-        function movePageDown(e){
-            e.preventDefault();            
+    movePageDown(e){
+        e.preventDefault();            
             $('html, body').animate({
                 scrollTop: $("#buffer-zone-div").offset().top
             }, 1000);
-        }
-        
+    }
+    
+    render() {        
         return(
         <div>
             <div className="intro-div">
@@ -21,7 +21,7 @@ export default class Intro extends Component {
                 <div className="text-container">
                     <h1 id="the-winter-war" className="text-center" role="banner" tabIndex="0">The Winter War</h1>
                     <h2 id="the-story" className="text-center">The story of how Finland held its own against the Soviet Union</h2>
-                    <h1 id="down-arrow" className="text-center" tabIndex="0" onClick={movePageDown}>|<br/>v</h1>
+                    <h1 id="down-arrow" className="text-center" tabIndex="0" onClick={this.movePageDown}>|<br/>v</h1>
                 </div>
             </div>
         </div>
